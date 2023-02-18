c9807cf5beb0086f794b642b9aa6ae06f14a3b35
2toesxtra <devkelly9622@gmail.com>
2023-02-17 19:14:43 -0500

    no sign up or sign in on side nav

--- src/widgets/layout/sidenav.jsx
+++ src/widgets/layout/sidenav.jsx
@@ -57,7 +57,8 @@ export function Sidenav({ brandImg, brandName, routes }) {
       </div>
       <div className="m-4">
         {routes.map(({ layout, title, pages }, key) => (
-          <ul key={key} className="mb-4 flex flex-col gap-1">
+          <>
+          {title!=="auth pages"&&<ul key={key} className="mb-4 flex flex-col gap-1">
             {title && (
               <li className="mx-3.5 mt-4 mb-2">
                 <Typography
@@ -97,7 +98,7 @@ export function Sidenav({ brandImg, brandName, routes }) {
                 </NavLink>
               </li>
             ))}
-          </ul>
+          </ul>}</>
         ))}
       </div>
     </aside>
